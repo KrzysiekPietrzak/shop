b5a29656fc353cb3d406c2a32df54939db6b2147
KrzysiekPietrzak <krzysztof.pietrzak17@gmail.com>
2023-03-27 19:18:39 +0200

    crud

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a  class="dropdown-item" href="/users/list">Użytkownicy</a>    
+                                <a  class="dropdown-item" href="{{route('products.index')}}">Produkty</a>    
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -87,11 +88,12 @@
 
  
 <script type="text/javascript">
- 
-@yield('javascript')
+ @yield('javascript')
 </script>
 
+<script type="text/javascript">
 @yield('js-files')
- 
-    </body>
+ </script>
+
+</body>
 </html>
